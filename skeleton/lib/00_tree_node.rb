98473975aba10bc8e7b162904f10c3bfeb1e0f22
Cory Boris <tromboris@gmail.com>
2023-01-27 14:02:04 -0500

    Define first two methods

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = [] 
+    end
 
+
+
+    def parent=(parent)
+        @parent = parent
+        if parent != nil
+            self.parent.children
+        end
+    end
+ 
+    attr_reader :parent, :children, :value
 end
