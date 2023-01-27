de210b1db965b283a6634f697ad58b79817d427a
Cory Boris <tromboris@gmail.com>
2023-01-27 16:48:59 -0500

    try making progress

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -12,7 +12,9 @@ class PolyTreeNode
     def parent=(passed_node)
         # debugger
         old_parent = @parent
+        debugger
         @parent = passed_node
+
         if @parent != nil
             if !@parent.children.include?(self)
                 @parent.children << self
@@ -20,10 +22,17 @@ class PolyTreeNode
                 @parent.children.delete(self) until @parent.children.count(self) == 1 
             end 
         end
+        
+        debugger
         if old_parent != nil
             old_parent.children.delete(self) until old_parent.children.count(self) == 0
         end
+
     end
  
     attr_reader :parent, :children, :value
+    
+    def inspect
+        "value: #{value}, parent: #{parent ? parent.value : "root"}, children: #{children.map(&:value)}"
+    end
 end
